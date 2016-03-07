3c666315cf0add1a811f35f3a13606d5ef21a096
Shayne Torres <shayneptorres@gmail.com>
2016-03-07 10:50:41 -0800

    begin implementing game

--- UpDownLeftRight/ViewController.swift
+++ UpDownLeftRight/ViewController.swift
@@ -9,17 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    override func viewDidLoad(){
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        leftSwipe.direction = .Left
+        self.view!.addGestureRecognizer(leftSwipe)
+        
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        rightSwipe.direction = .Right
+        self.view!.addGestureRecognizer(rightSwipe)
+        
+        let upSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        upSwipe.direction = .Up
+        self.view!.addGestureRecognizer(upSwipe)
+        
+        let downSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        downSwipe.direction = .Down
+        self.view!.addGestureRecognizer(downSwipe)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var swipeDirection: String = ""
+    var timerCount: Int = 0
+    var timer = NSTimer()
+    
+    
+    @IBOutlet weak var testLabel: UILabel!
+    
+    func handleSwipes(sender: UISwipeGestureRecognizer){
+        if sender.direction == .Up {
+            swipeDirection = "Up"
+        } else if sender.direction == .Down {
+            swipeDirection = "Down"
+        } else if sender.direction == .Left {
+            swipeDirection = "Left"
+        } else if sender.direction == .Right {
+            swipeDirection = "Right"
+        }
     }
-
-
+    
+    func generateNextDirection(){
+    let randInt = String(arc4random_uniform(5))
+        if randInt == "1" {
+            testLabel.text = "UP"
+        } else if randInt == "2" {
+            testLabel.text = "DOWN"
+        } else if randInt == "3" {
+            testLabel.text = "LEFT"
+        } else if randInt == "4" {
+            testLabel.text = "RIGHT"
+        }
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: "update", userInfo: nil, repeats: true)
+    }
+    
+    func update(){
+        timerCount++
+        if timerCount > 5 {
+            timer.invalidate()
+            generateNextDirection()
+        }
+    }
+    
+    @IBAction func playGame(sender: UIButton) {
+        generateNextDirection()
+    }
 }
-
