f01abb62d8e659e63a5868750778853167430f20
Shayne Torres <shayneptorres@gmail.com>
2016-03-06 23:39:57 -0800

    Initial Commit

---
+++ UpDownLeftRight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UpDownLeftRight
+//
+//  Created by Shayne Torres on 3/6/16.
+//  Copyright © 2016 Shayne Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
